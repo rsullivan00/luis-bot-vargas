90d6bff0d6aa4ed75c5a08610ad4a937af9973f4
Rick Sullivan <rsullivan@focusvision.com>
2019-09-19 13:09:09 -0700

    Add score cleaning

--- src/analyze.py
+++ src/analyze.py
@@ -5,3 +5,9 @@ all_cards = all_cards[all_cards.lang == 'en']
 lsv_scores = pd.read_csv('data/lsv_scores.csv')
 
 cards = lsv_scores.merge(all_cards, on=['set', 'name'])
+
+# For now, choose the lower bound of build-around cards as our score
+cards['score_clean'] = cards.score.str.replace(r'([0-5]\.[05]).*', lambda m: m.group(1))
+cards['score_clean'] = pd.to_numeric(cards.score_clean)
+
+import pdb; pdb.set_trace()
