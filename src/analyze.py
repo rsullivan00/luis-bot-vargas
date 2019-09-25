c30cd0e23ea209c3ab321870b1424ab80ef166bc
Rick Sullivan <rsullivan@focusvision.com>
2019-09-24 22:33:03 -0700

    Get super hacky text embedding features running

--- src/analyze.py
+++ src/analyze.py
@@ -9,7 +9,7 @@ all_cards = pd.read_json("data/scryfall-default-cards.json")
 all_cards = all_cards[all_cards.lang == "en"]
 lsv_scores = pd.read_csv("data/lsv_scores.csv")
 
-cards = lsv_scores.merge(all_cards, on=["set", "name"])[:10]
+cards = lsv_scores.merge(all_cards, on=["set", "name"])
 
 # Handle `:` -> `.` typo...c'mon LSV
 cards["score_clean"] = cards.score.str.replace(
@@ -30,6 +30,25 @@ cards["build_around"] = cards.score.str.contains("/")
 # mana_costs_split = cards.mana_cost.apply(lambda mc: re.findall(r"{(.+?)}", mc))
 
 
+bert = BertEmbedding()
+
+
+def embed_text(text):
+    embedding = bert([text])[0][1]
+    if embedding is not None:
+        return np.array(pd.Series(embedding).mean())
+    return np.zeros(768)
+
+
+embedded_oracle_text = (
+    cards.oracle_text.fillna("").map(embed_text).fillna(0)
+)  # Do I want to do this `fillna(0)`?
+features = pd.DataFrame.from_items(
+    zip(embedded_oracle_text.index, embedded_oracle_text.values)
+).T
+features["power"] = pd.to_numeric(cards.power, errors="coerce").fillna(0)
+features["toughness"] = pd.to_numeric(cards.toughness, errors="coerce").fillna(0)
+
 type_line_dummies = cards.type_line.str.get_dummies(" ").add_prefix("type_line_")
 rarity_dummies = cards.rarity.str.get_dummies().add_prefix("rarity_")
 
@@ -39,7 +58,8 @@ toughness_dummies = cards.toughness.str.get_dummies().add_prefix("toughness_")
 
 features = pd.concat(
     [
-        cards[["score_clean", "cmc"]],
+        features,
+        cards.cmc,
         rarity_dummies,
         type_line_dummies,
         power_dummies,
@@ -47,41 +67,45 @@ features = pd.concat(
     ],
     axis=1,
 )
-bert = BertEmbedding()
-# features["oracle_text"] = cards.oracle_text.fillna("").map(lambda x: bert([x])[0][1])
-# features["oracle_text"] = (
-#     cards.oracle_text.fillna("")
-#     .str.split("\n")
-#     .map(lambda x: [embedding for _sentence_items, embedding in bert(x)])
-# )
-def embed_text(text):
-    embedding = bert([text])[0][1]
-    if embedding is not None:
-        return np.array(pd.Series(embedding).mean())
-    return np.zeros(768)
+# Something about index 2377 causes
+# *** ValueError: Input contains NaN, infinity or a value too large for dtype('float64').
+# The data appears fine on inspection, though.
+# (Pdb) train[-454:-453].isna().any(1)
+# 2377    False
+# (Pdb) train[-454:-453].max(axis=1)
+# 2377    2.0
+# dtype: float64
+# (Pdb) train[-454:-453].min(axis=1)
+# 2377   -4.035182
+#
+# Edit: Turns out that `score_clean` is NaN, should drop those rows going forward
 
-
-features["oracle_text"] = cards.oracle_text.fillna("").map(embed_text)
-features["power"] = pd.to_numeric(cards.power, errors="coerce").fillna(0)
-features["toughness"] = pd.to_numeric(cards.toughness, errors="coerce").fillna(0)
-features = features.dropna()
+features = features.drop(2377)
+features.dropna()
 train, test = train_test_split(features, test_size=0.2)
 
 
 # Train on `train`
 
-import pdb
+try:
+    model = GradientBoostingRegressor().fit(train, cards.score_clean.loc[train.index])
+except:
+    import pdb
 
-pdb.set_trace()
-model = GradientBoostingRegressor().fit(train.drop("score_clean", 1), train.score_clean)
+    pdb.set_trace()
+
 
 #
 # Evaluate `test` into `test.prediction`
 
-test["prediction"] = model.predict(test.drop("score_clean", 1))
+test["prediction"] = model.predict(test)
 test["prediction"] = test.prediction.clip(upper=5.0, lower=0.0)
 
 # Score RMSE
 
-rmse = ((test.prediction - test.score_clean) ** 2).mean() ** 0.5
+rmse = ((test.prediction - cards.score_clean.loc[test.index]) ** 2).mean() ** 0.5
 print("RMSE: {}".format(rmse))
+
+import pdb
+
+pdb.set_trace()
